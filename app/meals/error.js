b3908858899003a1c1344b1c5548aa3ae365f413
sunny <sunny@bytespark.app>
2024-09-14 14:35:29 +0800

    add meals grid loading, error handler and global not found handler

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>Failed to fetch meals</h1>
+      <p>Please try again later.</p>
+    </main>
+  );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,30 +1,35 @@
-import Link from 'next/link';
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import Link from "next/link";
+import { Suspense } from "react";
+import MealsLoading from "./meals-loading";
+import classes from "./page.module.css";
 
-import classes from './page.module.css';
-import MealsGrid from '@/components/meals/meals-grid';
-import { getMeals } from '@/lib/meals';
-
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created{' '}
+          Delicious meals, created{" "}
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href="/meals/share">
-            Share Your Favorite Recipe
-          </Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoading />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
