126bb7ab6c341c48fa5128f074dfdeae713b422b
sunny <sunny@bytespark.app>
2024-09-14 16:16:11 +0800

    add server validate form

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to create the meal.</p>
+    </main>
+  );
+}
